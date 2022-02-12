8faab2c3b05248d5902c128c16ced206e6354dd8
Ilia Melnik <65551632+MelnikIlia@users.noreply.github.com>
2022-02-12 18:51:29 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,44 +1,61 @@
-let wow = new WOW({mobile:false});
+window.addEventListener('load', function () {
+  (function () {
+    const ibg = document.querySelectorAll('.ibg');
+    ibg.forEach(function (item) {
+      if (item.querySelector('img').length > 0) {
+        item.css('background-image', 'url("' + item.querySelector('img').attr('src') + '")');
+      }
+    });
+  })();
 
-wow.init();
+  const wow = new WOW({ mobile: false });
 
-(function ibg() {
-	$.each($(".ibg"), function (index, val) {
-		if ($(this).find("img").length > 0) {
-			$(this).css(
-				"background-image",
-				'url("' + $(this).find("img").attr("src") + '")'
-			);
-		}
-	});
-})();
+  wow.init();
 
-$("#signIn").click(function () {
-	$(".modal-register").fadeIn("300").css("display", "flex");
-	$(".form-signin").delay("200").addClass("showed");
-	$("body").addClass("lock");
-});
+  const modalRegistrationBack    = document.querySelector('.modal-register');
+  const closeRegistration        = document.querySelectorAll('.modal-close');
+  const signInButton             = document.getElementById('signIn');
+  const signUpButton             = document.getElementById('signUp');
+  const tabs                     = document.querySelectorAll('.tabs-menu');
 
-$("#signUp").click(function () {
-	$(".modal-register").fadeIn("300").css("display", "flex");
-	$(".form-sign-up").delay("200").addClass("showed");
-	$("body").addClass("lock");
-});
+  const showRegistration = (element) => {
+    modalRegistrationBack.classList.add('show');
+    setTimeout(function () {
+      document.querySelector(element).classList.add('showed');
+    }, 200);
+    document.body.style.width = `${document.documentElement.clientWidth}px`;
+    document.body.classList.add('lock');
+  };
 
-$(".modal-close").click(function () {
-	$(".form-signin, .form-sign-up").removeClass("showed");
-	$(".modal-register").fadeOut("300");
-	$("body").removeClass("lock");
-});
+  const hideRegistration = () => {
+    document.querySelector('.modal-register .showed').classList.remove('showed');
+    setTimeout(function () {
+      modalRegistrationBack.classList.remove('show');
+    }, 200);
+    document.body.classList.remove('lock');
+    document.body.style.width = `auto`;
+  };
 
-$('.tabs-menu[data-num="1"] .tab__button').click(function (event) {
-	event.preventDefault();
-	$('.tabs-menu[data-num="1"] .tab__button').parent().removeClass("active");
-	$(this).parent().toggleClass("active");
-});
+  signInButton.addEventListener('click', () => {
+    showRegistration('.form-sign-in');
+  });
 
-$('.tabs-menu[data-num="2"] .tab__button').click(function (event) {
-	event.preventDefault();
-	$('.tabs-menu[data-num="2"] .tab__button').parent().removeClass("active");
-	$(this).parent().toggleClass("active");
+  signUpButton.addEventListener('click', () => {
+    showRegistration('.form-sign-up');
+  });
+
+  closeRegistration.forEach((item) => {
+    item.addEventListener('click', hideRegistration);
+  });
+
+  const switchTabs = (tab) => {
+    tab.closest('.tabs-menu').querySelector('.active').classList.remove('active');
+    tab.parentElement.classList.toggle('active');
+  };
+
+  tabs.forEach((item) => {
+    item.addEventListener('click', (e) => {
+      switchTabs(e.target);
+    });
+  })
 });
